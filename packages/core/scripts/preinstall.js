567c49748a0c8fc60a8d8286fc876222b5a98eb7
Ashraf Reda <ashraf.reda.m@gmail.com>
2025-09-02 13:06:15 +0300

    starter-kit only

--- packages/core/scripts/preinstall.js
+++
@@ -1,5 +0,0 @@
-if (process.env.npm_execpath.indexOf('pnpm') === -1) {
-  console.error('Please use pnpm to install dependencies in this repository');
-  console.error('Installation using npm or yarn is not supported');
-  process.exit(1);
-}
